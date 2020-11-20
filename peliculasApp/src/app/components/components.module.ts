da4da8fdf99e03db042adf16740afc8bcc882989
Isanor Alejandro Lopez Molina <isanoralejandro@gmail.com>
2020-11-20 16:57:36 -0700

    A_PeliculasApp2.0[ - implementacion de Swiper ]

--- peliculasApp/src/app/components/components.module.ts
+++ peliculasApp/src/app/components/components.module.ts
@@ -3,17 +3,20 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 import { NavbarComponent } from './navbar/navbar.component';
-
-
+import { SlideshowComponent } from './slideshow/slideshow.component';
 
 @NgModule({
-  declarations: [NavbarComponent],
+  declarations: [
+    NavbarComponent,
+    SlideshowComponent
+  ],
   imports: [
     CommonModule,
     RouterModule
   ],
   exports: [
-    NavbarComponent
+    NavbarComponent,
+    SlideshowComponent
   ]
 })
 export class ComponentsModule { }
--- peliculasApp/src/app/pages/pages.module.ts
+++ peliculasApp/src/app/pages/pages.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { PeliculaComponent } from './pelicula/pelicula.component';
 import { BuscarComponent } from './buscar/buscar.component';
+import { ComponentsModule } from '../components/components.module';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import { BuscarComponent } from './buscar/buscar.component';
     BuscarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ComponentsModule
   ]
 })
 export class PagesModule { }
