b909e9371f92c3a31e8852adc0987c5aaf66502b
Isanor Alejandro Lopez Molina <isanoralejandro@gmail.com>
2020-11-24 15:24:41 -0700

    A_PeliculasApp2.0[ - CombineLatest - Animaciones Css ]

--- peliculasApp/src/app/pages/pelicula/pelicula.component.ts
+++ peliculasApp/src/app/pages/pelicula/pelicula.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Location } from '@angular/common';
+import { combineLatest } from 'rxjs';
 
 import { PeliculasService } from '../../services/peliculas.service';
 import { MovieResponse } from '../../interfaces/movieResponse';
 import { Cast } from '../../interfaces/creditsResponse';
+
 
 @Component({
   selector: 'app-pelicula',
@@ -22,8 +24,21 @@ export class PeliculaComponent implements OnInit {
               private router: Router) { }
 
   ngOnInit(): void {
+
     const id = this.activatedRoute.snapshot.params.id;
 
+    combineLatest([
+      this.peliculasService.getPeliculaDetalle(id),
+      this.peliculasService.getCast(id)
+    ]).subscribe( ([pelicula, cast]) => {
+      if (!pelicula) {
+        this.router.navigateByUrl('/home');
+        return;
+      }
+      this.pelicula = pelicula;
+      this.cast = cast.filter( actor => actor.profile_path !== null);
+    });
+    /*
     this.peliculasService.getPeliculaDetalle(id).subscribe( movie => {
       if (!movie) {
         this.router.navigateByUrl('/home');
@@ -34,7 +49,7 @@ export class PeliculaComponent implements OnInit {
 
     this.peliculasService.getCast(id).subscribe( cast => {
       this.cast = cast.filter( actor => actor.profile_path !== null);
-    });
+    }); */
   }
 
   onRegresar(): void {
